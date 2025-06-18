f8fc7b4d13c37c1c89f7fc16a980c404b65937a4
eric-moreira <eric.ufjf@gmail.com>
2025-06-18 20:06:32 +0000

    Modular code, isolating parsing for better readability

--- include/executor.h
+++ include/executor.h
@@ -2,5 +2,10 @@
 #define EXECUTOR_H
 
 void execute_command(const char *input);
+int parse_command(const char *input, char *input_copy, char **args, int *is_background);
+void setup_child_signals(void);
+void launch_process(char **args, int is_background);
 
 #endif
+
+
--- src/executor.c
+++ src/executor.c
@@ -4,39 +4,59 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-void execute_command(const char *input) {
-    char *args[128];
-    char *token;
-    char *input_copy = strdup(input);
-    int is_background = 0;
-    int i = 0;
+#define PARSE_SUCCESS 0
+#define PARSE_EMPTY 1
 
-    token = strtok(input_copy, " ");
+int parse_command(const char *input, char **input_copy, char **args, int *is_background){
+    char *token;
+    int i = 0;
+    
+    *input_copy = strdup(input);
+    if (*input_copy == NULL) {
+        perror("strdup failed");
+        return PARSE_EMPTY;
+    }
+
+    token = strtok(*input_copy, " ");
+
     while (token != NULL && i < 127) {
         args[i++] = token;
         token = strtok(NULL, " ");
     }
     if (i > 0 && strcmp(args[i - 1], "&") == 0) {
-        is_background = 1;
+        *is_background = 1;
         args[i - 1] = NULL;  // Remover o "&" da lista
     }
     args[i] = NULL;
 
     if (args[0] == NULL) {
-        free(input_copy);
-        return;
+        return PARSE_EMPTY;
     }
 
     if (strcmp(args[0], "&") == 0) {
-    // Usuário só digitou "&" como comando, ignora
-    return;
+        return PARSE_EMPTY;
+    }
+    return PARSE_SUCCESS;
 }
+
+void execute_command(const char *input) {    
+    int is_background = 0;
+    char *args[128];
+    int *bg = &is_background;
+    char *input_copy;
+
+
+    if(parse_command(input, &input_copy, args, bg)) {
+        return;
+    }
+
     pid_t pid = fork();
 
     if (pid == 0) {
         // Processo filho
         signal(SIGINT, SIG_DFL);
         execvp(args[0], args);
+        fprintf(stderr, "tinysh: command not found: %s\n", args[0]);
         perror("exec failed");
         exit(EXIT_FAILURE);
     } else if (pid > 0) {
@@ -49,6 +69,5 @@ void execute_command(const char *input) {
     } else {
         perror("fork failed");
     }
-
     free(input_copy);
 }
